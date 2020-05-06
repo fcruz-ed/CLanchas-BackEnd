265be39314b72a018c59141663c835ca7440ab3e
e <plasmasorrow@gmail.om>
2020-05-05 19:48:28 -0500

    SwaggerConfig.java mensaje ApiInfo agregado

--- src/main/java/com/clanchas/clanchas/config/SwaggerConfig.java
+++ src/main/java/com/clanchas/clanchas/config/SwaggerConfig.java
@@ -1,9 +1,11 @@
 package com.clanchas.clanchas.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.bind.annotation.RestController;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
@@ -11,10 +13,25 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @Configuration
 @EnableSwagger2
 public class SwaggerConfig {
+
+    @Bean
     public Docket documentation() {
         return new Docket(DocumentationType.SWAGGER_2).select()
                 .apis(RequestHandlerSelectors.withClassAnnotation(RestController.class))
                 .paths(PathSelectors.any())
-                .build();
+                .build()
+                .apiInfo(getApiInfo());
+    }
+
+    private ApiInfo getApiInfo() {
+        return new ApiInfo(
+                "CLanchas documentación del servicio REST BackEnd",
+                "Esta es la documentación de la API REST de Spring CLanchas BackEnd.",
+                "0.1",
+                "CLanchas BackEnd terminos de servicio",
+                "https://www.facebook.com/plasmasorrow",
+                "Licencia por definir",
+                "---"
+        );
     }
 }
