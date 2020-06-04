cce4f28217ebab3cff154f644c2f2e95cfffd438
e <plasmasorrow@gmail.om>
2020-06-04 17:58:54 -0500

    .

---
+++ src/main/java/com/clanchas/clanchas/controller/ExceptionControllerAdvice.java
@@ -0,0 +1,39 @@
+package com.clanchas.clanchas.controller;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class ExceptionControllerAdvice {
+
+    private final Log log = LogFactory.getLog(getClass());
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<String> exception(Exception e) {
+        ObjectMapper mapper = new ObjectMapper();
+        ErrorInfo errorInfo = new ErrorInfo(e);
+        String respJSONstring = "{}";
+        try {
+            respJSONstring = mapper.writeValueAsString(errorInfo);
+        } catch (JsonProcessingException err) {
+            log.error("Error al convertir mapper a JSON");
+            log.error(err.getMessage());
+        }
+        return ResponseEntity.badRequest().body(respJSONstring);
+    }
+
+    private class ErrorInfo {
+        public final String className;
+        public final String exMessage;
+
+        public ErrorInfo(Exception exception) {
+            this.className = exception.getClass().getName();
+            this.exMessage = exception.getLocalizedMessage();
+        }
+    }
+}
---
+++ src/main/resources/test-mysql.sql
@@ -0,0 +1,159 @@
+-- MySQL dump 10.13  Distrib 8.0.19, for Win64 (x86_64)
+--
+-- Host: localhost    Database: db_lanchas_test
+-- ------------------------------------------------------
+-- Server version	8.0.18
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `diario`
+--
+
+DROP TABLE IF EXISTS `diario`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `diario` (
+                          `id` int(11) NOT NULL AUTO_INCREMENT,
+                          `dia` varchar(10) DEFAULT NULL,
+                          `descripcion` varchar(500) DEFAULT NULL,
+                          PRIMARY KEY (`id`),
+                          UNIQUE KEY `diario_dia_uindex` (`dia`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `lancha`
+--
+
+DROP TABLE IF EXISTS `lancha`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `lancha` (
+                          `id` int(11) NOT NULL AUTO_INCREMENT,
+                          `numero` int(11) NOT NULL,
+                          `nombre` varchar(50) DEFAULT '',
+                          `estado` tinyint(1) DEFAULT '0',
+                          PRIMARY KEY (`id`),
+                          UNIQUE KEY `numero` (`numero`)
+) ENGINE=InnoDB AUTO_INCREMENT=35 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `lancha_rentada`
+--
+
+DROP TABLE IF EXISTS `lancha_rentada`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `lancha_rentada` (
+                                  `id` int(11) NOT NULL AUTO_INCREMENT,
+                                  `lancha_id` int(11) NOT NULL,
+                                  `renta_de` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+                                  `fecha` varchar(10) NOT NULL,
+                                  `en_uso` tinyint(1) DEFAULT '1',
+                                  `c_adultos` int(11) DEFAULT '0',
+                                  `c_jovenes` int(11) DEFAULT '0',
+                                  `observaciones` varchar(500) DEFAULT NULL,
+                                  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=62 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `persona_espera`
+--
+
+DROP TABLE IF EXISTS `persona_espera`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `persona_espera` (
+                                  `id` int(11) NOT NULL AUTO_INCREMENT,
+                                  `nombre` varchar(40) DEFAULT NULL,
+                                  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `tabulador_precios`
+--
+
+DROP TABLE IF EXISTS `tabulador_precios`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `tabulador_precios` (
+                                     `id` int(11) NOT NULL AUTO_INCREMENT,
+                                     `tiempo` varchar(10) NOT NULL,
+                                     `precio` decimal(8,2) DEFAULT NULL,
+                                     `estado` tinyint(4) DEFAULT '1',
+                                     `fecha` varchar(10) NOT NULL,
+                                     PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=29 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `usos`
+--
+
+DROP TABLE IF EXISTS `usos`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `usos` (
+                        `id` int(11) NOT NULL AUTO_INCREMENT,
+                        `lancha_rentada_id` int(11) NOT NULL,
+                        `tiempo` varchar(10) NOT NULL,
+                        `precio` decimal(8,2) NOT NULL,
+                        PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=51 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `usuario`
+--
+
+DROP TABLE IF EXISTS `usuario`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `usuario` (
+                           `id` int(11) NOT NULL AUTO_INCREMENT,
+                           `correo` varchar(50) NOT NULL,
+                           `clave` varchar(500) DEFAULT NULL,
+                           PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `utilidades`
+--
+
+DROP TABLE IF EXISTS `utilidades`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `utilidades` (
+                              `id` varchar(15) NOT NULL,
+                              `contenido` mediumtext,
+                              PRIMARY KEY (`id`),
+                              UNIQUE KEY `utilidades_id_uindex` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2020-06-04 12:40:04
+
+#INSERT INTO `lancha` VALUES (1,1,'Lancha #1',0),(2,2,'2- Lancha grande',0),(3,3,'3 - Lancha Pequeña',0);
